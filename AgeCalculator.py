60d04cce2c6fe3fd8e03120c7783184fd5883a39
vaishali2005 <vaishalikushwaha0406@gmail.com>
2025-07-03 15:49:41 +0530

    remove user define convert date to integer

--- AgeCalculator.py
+++ AgeCalculator.py
@@ -2,20 +2,6 @@ import datetime
 print("Welcome To Age Calculator")
 DOB = input("Enetr your date of birth (YYYY-MM-DD) : ")
 
-#user define date to int 
-# def DateConverter(d):
-#     dob = d + '-'
-#     date = ''
-#     list_dob = []
-#     for x in dob:
-#         # print(x)
-#         if x != '-':    
-#             date += x
-#         else: 
-#             list_dob.append(int(date))
-#             date = ''
-#     print(list_dob)
-
 #date to int converter by suing built in function
 def DateInt(dob):
     return  map(int,dob.split("-"))
