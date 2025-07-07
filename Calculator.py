4a8f737d4c795f974ad810ad6c29903901df4ae6
vaishali2005 <vaishalikushwaha0406@gmail.com>
2025-07-07 16:18:35 +0530

    make it simple

--- Calculator.py
+++ Calculator.py
@@ -17,115 +17,20 @@ class Calculator:
         return div
     def __pow__(self):
         pow = self.x ** self.y
-        return pow
-    
-def AddRemove(o1,o2,para):
-    expression.remove(o2)
-    expression.remove(o1)
-    expression.remove(o)                
-    for z in expression:
-        if  z not in new_exp:
-            new_exp.append(z)
-    new_exp.append(para)       
-operators = ['()', '**', '/', '*', '+', '-']
-# expression = []
-expression = [4.0, '*', 9.0, '+', 6.0, '/', 3.0]
-operand, operator = 0,''
-
-print(expression)
-# print(expression[0],expression[6])
-new_exp = []
-for o in operators:
-        if o in expression:
-            match o:
-                case '**':
-                    o1 = expression[expression.index(o)-1]
-                    o2 = expression[expression.index(o)+1]
-                    para = o1  ** o2
-                    AddRemove(o1,o2,para)
-                    break
-                case '/':
-                    o1 = expression[expression.index(o)-1]
-                    o2 = expression[expression.index(o)+1]
-                    para = o1  / o2
-                    AddRemove(o1,o2,para)
-                    # break
-                case '*': 
-                    o1 = expression[expression.index(o)-1]
-                    o2 = expression[expression.index(o)+1]
-                    para = o1  * o2
-                    AddRemove(o1,o2,para)
-                    # break
-                case '+':
-                    o1 = expression[expression.index(o)-1]
-                    o2 = expression[expression.index(o)+1]
-                    para = o1  + o2
-                    AddRemove(o1,o2,para)
-                    break
-                case '-':
-                    o1 = expression[expression.index(o)-1]
-                    o2 = expression[expression.index(o)+1]
-                    para = o1  - o2
-                    AddRemove(o1,o2,para)
-                    break
-print(new_exp)
-# print(expression)
-     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-        
-# print("Enter your calculation")
-# exp1 = float(input())
-# operator = input()
-# exp2 = float(input())
-# cal = Calculator()
-# match operator:
-#     case '+':
-#         print(exp1 + exp2)
-#     case '-':
-#         print(exp1 - exp2)
-#     case '*':
-#         print(exp1 * exp2)
-#     case '/':
-#         print(exp1 / exp2)
-#     case '**':
-#         print(exp1 ** exp2)
-
-
-
-
-
-# while True:
-#     if operator == '=':
-#         break
-#     else:
-#         operand = float(input())
-#         expression.append(operand)
-#         operator = input()
-#         expression.append(operator)
+        return pow        
+print("Enter your calculation")
+exp1 = float(input())
+operator = input()
+exp2 = float(input())
+cal = Calculator()
+match operator:
+    case '+':
+        print(exp1 + exp2)
+    case '-':
+        print(exp1 - exp2)
+    case '*':
+        print(exp1 * exp2)
+    case '/':
+        print(exp1 / exp2)
+    case '**':
+        print(exp1 ** exp2)
